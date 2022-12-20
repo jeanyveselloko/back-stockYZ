a4542461df52f6f03c4bea21e5868a1cd6fc6a7d
Jean Yves <jeanyvesellokopro.ea@gmail.com>
2022-12-20 15:20:56 +0100

    first commit

---
+++ src/main/java/com/stoks/stocksgestion/StocksGestionApplication.java
@@ -0,0 +1,13 @@
+package com.stoks.stocksgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksGestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stoks/stocksgestion/StocksGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoks.stocksgestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
